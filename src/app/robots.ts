10589013005f3abd83ace9c67784b218169180fd
himanshu jeswani <i.himanshujeswani@gmail.com>
2026-07-07 13:12:00 +0000

    Revert the changes from the last prompt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,1 @@
-import { MetadataRoute } from 'next';
-
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/admin',
-    },
-    sitemap: 'https://bharatpulse-sports.web.app/sitemap.xml',
-  };
-}
+// Robots.txt removed to revert SEO changes.
